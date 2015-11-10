07d3db7d378a1ff92e58ba4c621f7363bd3f34f1
Jeffiy <zhangmin6105@qq.com>
2015-11-10 16:45:02 +0800

    add update all pip packages script
    Signed-off-by: Jeffiy <zhangmin6105@qq.com>

---
+++ python/update-all-pip-packages.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding:utf-8
+
+import pip
+from subprocess import call
+
+for dist in pip.get_installed_distributions():
+    call("pip install --upgrade " + dist.project_name, shell=True)
